306a071e342d06a2227591cfb17d10021da7bc2f
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2024-04-24 20:40:29 -0400

    Solve Hipp Hipp

---
+++ hipphipp.java
@@ -0,0 +1,7 @@
+public class hipphipp {
+    public static void main(String[] args) {
+        for(int i = 0; i < 20; i++) {
+            System.out.println("Hipp hipp hurra!");
+        }
+    }
+}
